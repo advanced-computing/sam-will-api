9d8de605c0a298a493e73bbd69a0c3917f2533b9
WFrolich <whf2108@columbia.edu>
2025-03-13 10:56:48 -0400

    get data funciton

--- Fed_Data/functions.py
+++ Fed_Data/functions.py
@@ -5,10 +5,19 @@ def get_latest_data(pull_date):
 
     cpi_data = pd.read_csv("Fed_CPI.csv")
 
-    year = pull_date[2:3]
-    month = pull_date[5:6].int
+    year, month, day = pull_date.split('-')
+
+    year = year[2:]
+    month = month.lstrip('0')
+    # month = month.int()
+    # stringmonth = month.str()
+
+    # string_date = pull_date.dt.strftime('%YM%-m')
+    # year = string_date[2:4]
+    # month = string_date[5:7].astype(int)
     vintage = (f"PCPI{year}M{month}")
 
     cpi_data_vintage = cpi_data[["DATE", vintage]]
     
     return cpi_data_vintage
+   
